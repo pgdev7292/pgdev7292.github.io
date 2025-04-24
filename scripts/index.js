4c190d8e8f5c96ed415d307a7ffc877799b1436f
pgdev7292 <patbengled@icloud.com>
2025-04-24 11:02:29 -0700

    Added js.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,18 @@
 var backButton = document.getElementById("back_button");
 
+var attempts = 0;
+
 backButton.addEventListener("click", (e) => {
-    alert("nuh uh");
-    alert("Now you don't get a button!");
-    backButton.remove();
+    if (attempts < 1) {
+        alert("nuh uh");
+        alert("I know it's you, Orangelizard107")
+        alert("Now, you don't get a back button!");
+        backButton.textContent = "Click Me!";
+    } else {
+        alert("You don't quit, do ya?");
+        alert("I guess you don't want to have anything, Orangelizard107.")
+        document.body.innerHTML = "";
+    }
+
+    attempts++;
 });
